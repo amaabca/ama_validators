62f7a38f63ec6834c20695a2ed248599066eade5
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-01-28 09:05:27 -0700

    Added coveralls badge

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,11 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'simplecov'
 require 'ama_validators'
+require 'coveralls'
+Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
