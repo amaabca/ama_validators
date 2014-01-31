8b6fd14c7542ceca97262e3837465cbea45cd5c4
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-01-31 12:51:04 -0700

    Change postal code in e.g message
    * This probably have to change algo in the en.yml file for each project

--- lib/ama_validators/postal_code_format_validator.rb
+++ lib/ama_validators/postal_code_format_validator.rb
@@ -1,7 +1,7 @@
 class PostalCodeFormatValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
     unless value =~ /\A(\d{5}((-|\s)\d{4})?)|([txTX]\d[abceghjklmnprstvwxyzABCEGHJKLMNPRSTVWXYZ])\ {0,1}(\d[abceghjklmnprstvwxyzABCEGHJKLMNPRSTVWXYZ]\d)\z/
-      object.errors[attribute] << (options[:message] || "enter a valid AB or NT postal code (e.g. T2T 2T2)")
+      object.errors[attribute] << (options[:message] || "enter a valid AB or NT postal code (e.g. T4C 1A5)")
     end
   end
 end
