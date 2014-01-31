36b9c8199451424faeeb454f4d979f311bbfca66
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-01-31 15:11:31 -0700

    Change email validation regex to reject backticks
    * regex now will no accept backticks in any part of the email string

--- lib/ama_validators/email_format_validator.rb
+++ lib/ama_validators/email_format_validator.rb
@@ -1,6 +1,6 @@
 class EmailFormatValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    unless value =~ /\A[^@\s]+@([^@\s\.]+\.)+[^@\s\.]+\z/
+    unless value =~ /\A[^`@\s]+@([^@`\s\.]+\.)+[^`@\s\.]+\z/
       object.errors[attribute] << (options[:message] || "enter a valid email address (e.g. name@example.com)")
     end
   end
