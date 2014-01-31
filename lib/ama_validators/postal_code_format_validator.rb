a95c0c315c305f30cb09b2e24cf8f29eef314814
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-01-31 11:03:06 -0700

    Fix postal code regex
    * fix regex it was missing lowercase s and w

--- lib/ama_validators/postal_code_format_validator.rb
+++ lib/ama_validators/postal_code_format_validator.rb
@@ -1,6 +1,6 @@
 class PostalCodeFormatValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    unless value =~ /\A(\d{5}((-|\s)\d{4})?)|([txTX]\d[abceghjklmnprtvxyzABCEGHJKLMNPRSTVWXYZ])\ {0,1}(\d[abceghjklmnprstvxyzABCEGHJKLMNPRSTVWXYZ]\d)\z/
+    unless value =~ /\A(\d{5}((-|\s)\d{4})?)|([txTX]\d[abceghjklmnprstvwxyzABCEGHJKLMNPRSTVWXYZ])\ {0,1}(\d[abceghjklmnprstvwxyzABCEGHJKLMNPRSTVWXYZ]\d)\z/
       object.errors[attribute] << (options[:message] || "enter a valid AB or NT postal code (e.g. T2T 2T2)")
     end
   end
