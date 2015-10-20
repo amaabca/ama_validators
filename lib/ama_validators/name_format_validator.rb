9d01181e62d8078e9a50b2f05fbfb0f6817e1b49
Adam Melnyk <adam.melnyk@gmail.com>
2015-10-20 13:58:06 -0600

    Added tests

--- lib/ama_validators/name_format_validator.rb
+++ lib/ama_validators/name_format_validator.rb
@@ -1,6 +1,6 @@
 class NameFormatValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    unless value =~ /\A[a-zA-Z]+\z/
+    unless value =~ /\A[a-zA-Z,.'-]+\z/
       object.errors[attribute] << (options[:message] || "We're sorry your name cannot contain any special characters")
     end
   end
