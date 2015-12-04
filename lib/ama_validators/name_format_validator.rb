723ad3bea51f6708e17ebbb3bdc9f336440bd192
Adam Melnyk <adam.melnyk@gmail.com>
2015-12-03 17:03:26 -0700

    Added brackets into names
    * Currently there are over 1000 records in IMIS that contain parenthesis.
      When these users create an account and try to access anything the valiations
      on the member breaks.

--- lib/ama_validators/name_format_validator.rb
+++ lib/ama_validators/name_format_validator.rb
@@ -1,6 +1,6 @@
 class NameFormatValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    unless value =~ /\A[\sa-zA-ZÀàÂâÄäÈèÉéÊêËëÎîÏïÔôŒœÙùÛûÜüŸÿÇç,.'-]+\z/
+    unless value =~ /\A[\sa-zA-ZÀàÂâÄäÈèÉéÊêËëÎîÏïÔôŒœÙùÛûÜüŸÿÇç,.'\-\)\(]+\z/
       object.errors[attribute] << (options[:message] || "We're sorry your name cannot contain any special characters")
     end
   end
